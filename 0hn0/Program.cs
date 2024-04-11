5ed1a169cec959d363a62f54193bc05cd9aef531
Zadd ZC <davidbk927@gmail.com>
2024-04-11 15:43:12 +0200

    web driver cuz i can

--- 0hn0/Program.cs
+++ 0hn0/Program.cs
@@ -1,7 +1,25 @@
-﻿namespace _0hn0;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+
+namespace _0hn0;
 
 internal class Program {
+    public static int gridSize = 4;
     static void Main(string[] args) {
-        Console.WriteLine("Hello, World!");
+
+
+
+        WebDriver webDriver = new ChromeDriver();
+        webDriver.Navigate().GoToUrl("https://0hn0.com/");
+
+        bool isRunning = true;
+        while (isRunning) {
+            webDriver.ExecuteScript($"Game.startGame({gridSize},0)");
+            Thread.Sleep(1000);
+
+
+        }
+
     }
 }
